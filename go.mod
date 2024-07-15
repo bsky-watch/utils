71a2d56f70eec1764e3cb7d81117d84b2cafbee7
Max Ignatenko <gelraen.ua@gmail.com>
2024-07-15 11:10:59 +0100

    Add listserver

--- go.mod
+++ go.mod
@@ -1,40 +1,49 @@
 module bsky.watch/utils
 
-go 1.21
+go 1.22
+
+toolchain go1.22.2
 
 require (
-	github.com/bluesky-social/indigo v0.0.0-20231124230700-5bdc9f1949c9
-	github.com/gorilla/websocket v1.5.0
+	github.com/Jille/convreq v1.7.1
+	github.com/bluesky-social/indigo v0.0.0-20240712235331-7d1235931bd3
+	github.com/gorilla/websocket v1.5.1
 	github.com/ipfs/go-cid v0.4.1
 	github.com/rs/zerolog v1.29.1
 	github.com/urfave/cli/v2 v2.25.7
-	github.com/whyrusleeping/cbor-gen v0.0.0-20230818171029-f91ae536ca25
-	golang.org/x/oauth2 v0.10.0
+	github.com/whyrusleeping/cbor-gen v0.1.1-0.20240311221002-68b9f235c302
+	golang.org/x/oauth2 v0.12.0
 )
 
 require (
+	github.com/RussellLuo/slidingwindow v0.0.0-20200528002341-535bb99d338b // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/carlmjohnson/versioninfo v0.22.5 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
-	github.com/hashicorp/go-retryablehttp v0.7.4 // indirect
-	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.5 // indirect
+	github.com/hashicorp/golang-lru v1.0.2 // indirect
+	github.com/hashicorp/golang-lru/arc/v2 v2.0.6 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/ipfs/bbloom v0.0.4 // indirect
-	github.com/ipfs/go-block-format v0.1.2 // indirect
+	github.com/ipfs/go-block-format v0.2.0 // indirect
 	github.com/ipfs/go-blockservice v0.5.2 // indirect
 	github.com/ipfs/go-datastore v0.6.0 // indirect
 	github.com/ipfs/go-ipfs-blockstore v1.3.1 // indirect
 	github.com/ipfs/go-ipfs-ds-help v1.1.1 // indirect
 	github.com/ipfs/go-ipfs-exchange-interface v0.2.1 // indirect
 	github.com/ipfs/go-ipfs-util v0.0.3 // indirect
-	github.com/ipfs/go-ipld-cbor v0.0.7-0.20230126201833-a73d038d90bc // indirect
-	github.com/ipfs/go-ipld-format v0.5.0 // indirect
+	github.com/ipfs/go-ipld-cbor v0.1.0 // indirect
+	github.com/ipfs/go-ipld-format v0.6.0 // indirect
 	github.com/ipfs/go-ipld-legacy v0.2.1 // indirect
 	github.com/ipfs/go-libipfs v0.7.0 // indirect
 	github.com/ipfs/go-log v1.0.5 // indirect
@@ -43,16 +52,16 @@ require (
 	github.com/ipfs/go-metrics-interface v0.0.1 // indirect
 	github.com/ipfs/go-verifcid v0.0.3 // indirect
 	github.com/ipld/go-car v0.6.1 // indirect
-	github.com/ipld/go-car/v2 v2.10.1 // indirect
+	github.com/ipld/go-car/v2 v2.13.1 // indirect
 	github.com/ipld/go-codec-dagpb v1.6.0 // indirect
-	github.com/ipld/go-ipld-prime v0.20.0 // indirect
+	github.com/ipld/go-ipld-prime v0.21.0 // indirect
 	github.com/jbenet/goprocess v0.1.4 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/minio/sha256-simd v1.0.1 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/multiformats/go-base32 v0.1.0 // indirect
@@ -64,29 +73,28 @@ require (
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/petar/GoLLRB v0.0.0-20210522233825-ae3b015fd3e9 // indirect
 	github.com/polydawn/refmt v0.89.1-0.20221221234430-40501e09de1f // indirect
-	github.com/prometheus/client_golang v1.16.0 // indirect
-	github.com/prometheus/client_model v0.4.0 // indirect
-	github.com/prometheus/common v0.44.0 // indirect
-	github.com/prometheus/procfs v0.11.0 // indirect
+	github.com/prometheus/client_golang v1.17.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/whyrusleeping/cbor v0.0.0-20171005072247-63513f603b11 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	go.opentelemetry.io/otel v1.19.0 // indirect
-	go.opentelemetry.io/otel/metric v1.19.0 // indirect
-	go.opentelemetry.io/otel/trace v1.19.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.46.1 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.24.0 // indirect
-	golang.org/x/crypto v0.13.0 // indirect
-	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1 // indirect
-	golang.org/x/net v0.15.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
-	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
-	gorm.io/driver/sqlite v1.5.2 // indirect
-	gorm.io/gorm v1.25.2 // indirect
+	gorm.io/gorm v1.25.9 // indirect
 	lukechampine.com/blake3 v1.2.1 // indirect
 )
---
+++ listserver/main.go
@@ -0,0 +1,285 @@
+package listserver
+
+import (
+	"bytes"
+	"cmp"
+	"context"
+	"fmt"
+	"net/http"
+	"slices"
+	"strings"
+	"sync"
+
+	"github.com/Jille/convreq"
+	"github.com/Jille/convreq/respond"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	comatproto "github.com/bluesky-social/indigo/api/atproto"
+	"github.com/bluesky-social/indigo/api/bsky"
+	"github.com/bluesky-social/indigo/repo"
+	"github.com/bluesky-social/indigo/xrpc"
+	cborgen "github.com/whyrusleeping/cbor-gen"
+
+	"bsky.watch/utils/aturl"
+	"bsky.watch/utils/firehose"
+	"bsky.watch/utils/pagination"
+)
+
+type Request struct {
+	Subject string
+}
+
+type Response struct {
+	Subject   string            `json:"subject"`
+	Listitems map[string]string `json:"listitems"`
+	Rev       string            `json:"rev"`
+	Cid       string            `json:"cid"`
+}
+
+type entry struct {
+	List    string
+	Subject string
+}
+
+// Server keeps track of all `app.bsky.graph.listitem`s for a given account,
+// and responds to membership queries over HTTP.
+//
+// To ensure that you're not missing any records, you need to connect to firehose
+// *before* calling Sync().
+type Server struct {
+	client *xrpc.Client
+	did    string
+
+	handler http.HandlerFunc
+
+	mu                   sync.RWMutex
+	firehoseHookReturned bool
+	lastKnownRev         string
+	lastCid              string
+	// Don't try to do anything smart yet, just iterating over the map
+	// should be fast enough for now.
+	rkeyToEntry map[string]entry
+	initQueue   []*comatproto.SyncSubscribeRepos_Commit
+}
+
+func New(client *xrpc.Client, did string) *Server {
+	s := &Server{
+		client: client,
+		did:    did,
+	}
+	s.handler = convreq.Wrap(s.serveHTTP)
+	return s
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	s.handler(w, req)
+}
+
+func (s *Server) serveHTTP(ctx context.Context, req *http.Request) convreq.HttpResponse {
+	var input Request
+	input.Subject = req.FormValue("subject")
+	if input.Subject == "" {
+		return respond.BadRequest("missing 'subject'")
+	}
+
+	result := &Response{
+		Subject:   input.Subject,
+		Listitems: map[string]string{},
+	}
+
+	s.mu.RLock()
+	if s.lastKnownRev == "" {
+		s.mu.RUnlock()
+		return respond.TooEarly("server not ready yet")
+	}
+	for rkey, entry := range s.rkeyToEntry {
+		if entry.Subject == input.Subject {
+			result.Listitems[rkey] = entry.List
+		}
+	}
+	result.Rev = s.lastKnownRev
+	result.Cid = s.lastCid
+	s.mu.RUnlock()
+
+	return respond.JSON(result)
+}
+
+func (s *Server) UpdateFromFirehose() firehose.Hook {
+	s.mu.Lock()
+	s.firehoseHookReturned = true
+	s.mu.Unlock()
+
+	return firehose.Hook{
+		Predicate: firehose.From(s.did),
+		Action: func(ctx context.Context, commit *comatproto.SyncSubscribeRepos_Commit, _ *comatproto.SyncSubscribeRepos_RepoOp, _ cborgen.CBORMarshaler) {
+			log := zerolog.Ctx(ctx)
+			s.mu.Lock()
+			startingUp := false
+			alreadyProcessed := false
+			missingPreviousCommit := false
+			if s.lastKnownRev == "" {
+				startingUp = true
+				s.initQueue = append(s.initQueue, commit)
+			} else if commit.Rev == s.lastKnownRev {
+				alreadyProcessed = true
+			} else if commit.Since != nil && *commit.Since != s.lastKnownRev {
+				missingPreviousCommit = true
+			}
+			s.mu.Unlock()
+			if startingUp || alreadyProcessed {
+				return
+			}
+			if missingPreviousCommit {
+				log.Error().Msgf("We somehow missed the previous commit %q. List memberships may be inaccurate.", *commit.Since)
+			}
+
+			if err := s.processCommit(ctx, commit, false); err != nil {
+				log.Error().Err(err).Msgf("Failed to process commit %q: %s", commit.Rev, err)
+				return
+			}
+		},
+	}
+}
+
+func (s *Server) processCommit(ctx context.Context, commit *comatproto.SyncSubscribeRepos_Commit, locked bool) error {
+	repo_, err := repo.ReadRepoFromCar(ctx, bytes.NewReader(commit.Blocks))
+	if err != nil {
+		return fmt.Errorf("ReadRepoFromCar: %w", err)
+	}
+
+	insert := map[string]entry{}
+	remove := []string{}
+	for _, op := range commit.Ops {
+		parts := strings.SplitN(op.Path, "/", 2)
+		if len(parts) != 2 {
+			log.Error().Msgf("Unexpected number of parts in op.Path: %q", op.Path)
+			continue
+		}
+		if parts[0] != "app.bsky.graph.listitem" {
+			continue
+		}
+		rkey := parts[1]
+
+		switch op.Action {
+		case "create", "update":
+			_, rec, err := repo_.GetRecord(ctx, op.Path)
+			if err != nil {
+				return fmt.Errorf("failed to get record %q: %w", op.Path, err)
+			}
+
+			switch record := rec.(type) {
+			case *bsky.GraphListitem:
+				insert[rkey] = entry{
+					Subject: record.Subject,
+					List:    record.List,
+				}
+			default:
+				return fmt.Errorf("unexpected record type %T", record)
+			}
+		case "delete":
+			remove = append(remove, rkey)
+		default:
+			return fmt.Errorf("unknown action %q", op.Action)
+		}
+	}
+
+	if !locked {
+		s.mu.Lock()
+	}
+	for _, rkey := range remove {
+		delete(s.rkeyToEntry, rkey)
+	}
+	for k, v := range insert {
+		s.rkeyToEntry[k] = v
+	}
+	s.lastKnownRev = commit.Rev
+	s.lastCid = commit.Commit.String()
+	if !locked {
+		s.mu.Unlock()
+	}
+
+	return nil
+}
+
+func (s *Server) Sync(ctx context.Context) error {
+	s.mu.Lock()
+	ok := s.firehoseHookReturned
+	s.mu.Unlock()
+	if !ok {
+		return fmt.Errorf("must connect to firehose before calling Sync()")
+	}
+
+	status, err := comatproto.SyncGetLatestCommit(ctx, s.client, s.did)
+	if err != nil {
+		return fmt.Errorf("com.atproto.sync.getLatestCommit: %w", err)
+	}
+
+	if status.Rev == "" {
+		return fmt.Errorf("server did not return the current `rev` value")
+	}
+
+	// Changes happening after status.Rev might affect the result, but
+	// that's fine: duplicate mutations in s.initQueue will have no effect.
+	entries, err := pagination.Reduce(
+		// fetch
+		func(cursor string) (*comatproto.RepoListRecords_Output, string, error) {
+			resp, err := comatproto.RepoListRecords(ctx, s.client, "app.bsky.graph.listitem", cursor, 100, s.did, false, "", "")
+			cursor = ""
+			if resp != nil && resp.Cursor != nil {
+				cursor = *resp.Cursor
+			}
+			return resp, cursor, err
+		},
+		// combine
+		func(resp *comatproto.RepoListRecords_Output, acc map[string]entry) (map[string]entry, error) {
+			if acc == nil {
+				acc = map[string]entry{}
+			}
+			for _, rec := range resp.Records {
+				switch record := rec.Value.Val.(type) {
+				case *bsky.GraphListitem:
+					u, err := aturl.Parse(rec.Uri)
+					if err != nil {
+						return nil, fmt.Errorf("failed to parse %q: %w", rec.Uri, err)
+					}
+					parts := strings.SplitN(strings.TrimPrefix(u.Path, "/"), "/", 2)
+					if len(parts) < 2 {
+						return nil, fmt.Errorf("invalid URI %q", rec.Uri)
+					}
+					rkey := parts[1]
+
+					acc[rkey] = entry{
+						Subject: record.Subject,
+						List:    record.List,
+					}
+				}
+			}
+			return acc, nil
+		})
+	if err != nil {
+		return fmt.Errorf("failed to fetch listitems: %w", err)
+	}
+
+	s.mu.Lock()
+	s.rkeyToEntry = entries
+	s.lastKnownRev = status.Rev
+	s.lastCid = status.Cid
+
+	slices.SortFunc(s.initQueue, func(a, b *comatproto.SyncSubscribeRepos_Commit) int {
+		return cmp.Compare(a.Rev, b.Rev)
+	})
+	for _, commit := range s.initQueue {
+		if commit.Rev <= s.lastKnownRev {
+			continue
+		}
+		if err := s.processCommit(ctx, commit, true); err != nil {
+			return err
+		}
+	}
+	s.initQueue = nil
+
+	s.mu.Unlock()
+
+	return nil
+}
